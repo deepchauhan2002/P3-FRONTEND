be3308194adb70c5b5ce6029215fc9577cf4afb3
Deep Chauhan <ay.19ce09@thdcihet.ac.in>
2023-09-06 17:10:05 +0530

    add header

--- p5-/src/components/ResponsiveSketch.js
+++ p5-/src/components/ResponsiveSketch.js
@@ -1,66 +1,86 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import Sketch from 'react-p5';
 
-const ResponsiveSketch = () => {
-  let t = 0;
-  let words = ['HI I AM DEEP'];
-  let gapFactor = 1.5; // Adjust this value to control the gap between text
-  let speedFactor = 0.5; // Adjust this value to control the animation speed
+class Particle {
+  constructor(p5, type) {
+    this.type = type;
+    this.loc = p5.createVector(p5.random(p5.width * 1.2), p5.random(p5.height), 2);
+    this.angle = p5.random(p5.TWO_PI);
+    this.dir = p5.createVector(p5.cos(this.angle), p5.sin(this.angle));
+    this.speed = this.type === 'a' ? 0.5 : 0.75;
+    this.d = 1;
+  }
 
-  const [canvas, setCanvas] = useState(null);
+  move(p5) {
+    this.angle = p5.noise(this.loc.x / noiseScale, this.loc.y / noiseScale, p5.frameCount / noiseScale) * p5.TWO_PI * noiseStrength;
+    this.dir.x = p5.cos(this.angle) + p5.sin(this.angle) - p5.sin(this.angle);
+    this.dir.y = p5.sin(this.angle) - p5.cos(this.angle) * p5.sin(this.angle);
+    this.vel = this.dir.copy();
+    this.vel.mult(this.speed * this.d);
+    this.loc.add(this.vel);
+  }
 
+  checkEdges(p5) {
+    if (this.loc.x < 0 || this.loc.x > p5.width || this.loc.y < 0 || this.loc.y > p5.height) {
+      this.loc.x = p5.random(p5.width * 1.2);
+      this.loc.y = p5.random(p5.height);
+    }
+  }
+
+  update(p5, radius) {
+    p5.ellipse(this.loc.x, this.loc.y, radius);
+  }
+}
+
+let particles_a = [];
+let particles_b = [];
+let particles_c = [];
+const noiseScale = 300;
+const noiseStrength = 1.2;
+const num = 3000;
+const fade = 100; // Add fade variable
+const radius = 3; // Add radius variable
+
+const YourComponent = () => {
   const setup = (p5, canvasParentRef) => {
-    // Set canvas size to match the window size
-    const newCanvas = p5.createCanvas(window.innerWidth, window.innerHeight);
-    newCanvas.parent(canvasParentRef);
+    p5.createCanvas(1200, 900).parent(canvasParentRef);
+    p5.noStroke();
 
-    // Disable body overflow to prevent scrolling
-    document.body.style.overflow = 'hidden';
-
-    // Store the canvas reference
-    setCanvas(newCanvas);
-  };
-
-  const draw = (p5) => {
-    p5.background(0);
-    t += speedFactor; // Adjust animation speed
-
-    // Adjust the gap between text based on window size
-    let gap = window.innerWidth / 40 * gapFactor;
-
-    p5.translate(
-      window.innerWidth / 2 - (p5.textWidth(words) / 2 + gap),
-      0
-    );
-
-    for (var b = 0; b < 3; b++) {
-      for (var a = 0; a < 24; a++) {
-        var dx = 400 * p5.sin(p5.radians(p5.PI / 2 + t * 2 + a * 10));
-        p5.fill(255, 127, 1);
-        p5.textSize(50);
-        p5.textAlign(p5.CENTER);
-
-        p5.text(words, a * 50 + window.innerWidth / 1.6 * b + dx, a * 64);
-      }
+    for (let i = 0; i < num; i++) {
+      particles_a.push(new Particle(p5, 'a'));
+      particles_b.push(new Particle(p5, 'b'));
+      particles_c.push(new Particle(p5, 'c'));
     }
   };
 
-  // Resize the canvas when the window is resized
-  const windowResized = (p5) => {
-    // Access the canvas element and set its width and height
-    canvas.canvas.width = window.innerWidth;
-    canvas.canvas.height = window.innerHeight;
+  const draw = (p5) => {
+    p5.fill(0, 2);
+    p5.noStroke();
+    p5.rect(0, 0, p5.width, p5.height);
+
+    for (let i = 0; i < num; i++) {
+      p5.fill(255, fade); // Use the 'fade' variable
+      particles_a[i].move(p5);
+      particles_a[i].update(p5, radius); // Use the 'radius' variable
+      particles_a[i].checkEdges(p5);
+
+      p5.fill(0, 255, 255, fade); // Use the 'fade' variable
+      particles_b[i].move(p5);
+      particles_b[i].update(p5, radius); // Use the 'radius' variable
+      particles_b[i].checkEdges(p5);
+
+      p5.fill(102, 153, 255, fade); // Use the 'fade' variable
+      particles_c[i].move(p5);
+      particles_c[i].update(p5, radius); // Use the 'radius' variable
+      particles_c[i].checkEdges(p5);
+    }
   };
 
-  // Use useEffect to attach the window resize event listener
-  useEffect(() => {
-    window.addEventListener('resize', windowResized);
-    return () => {
-      window.removeEventListener('resize', windowResized);
-    };
-  }, [canvas]);
-
-  return <Sketch setup={setup} draw={draw} />;
+  return (
+    <div>
+      <Sketch setup={setup} draw={draw} />
+    </div>
+  );
 };
 
-export default ResponsiveSketch;
+export default YourComponent;
